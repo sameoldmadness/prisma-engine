037205a767e277ce0fce540f0c88c01530a4e63a
Tom Houlé <tom@tomhoule.com>
2019-09-23 11:07:45 +0200

    Use the correct database for introspection in tests

--- migration-engine/core/tests/test_harness/command_helpers.rs
+++ migration-engine/core/tests/test_harness/command_helpers.rs
@@ -1,4 +1,4 @@
-use super::introspect_database;
+use super::{introspect_database, TestSetup};
 use migration_connector::*;
 use migration_core::{api::GenericApi, commands::*};
 use sql_schema_describer::*;
@@ -9,11 +9,12 @@ pub struct InferAndApplyOutput {
     pub migration_output: MigrationStepsResultOutput,
 }
 
-pub fn infer_and_apply(api: &dyn GenericApi, datamodel: &str) -> InferAndApplyOutput {
-    infer_and_apply_with_migration_id(api, &datamodel, "the-migration-id")
+pub fn infer_and_apply(test_setup: &TestSetup, api: &dyn GenericApi, datamodel: &str) -> InferAndApplyOutput {
+    infer_and_apply_with_migration_id(test_setup, api, &datamodel, "the-migration-id")
 }
 
 pub fn infer_and_apply_with_migration_id(
+    test_setup: &TestSetup,
     api: &dyn GenericApi,
     datamodel: &str,
     migration_id: &str,
@@ -26,7 +27,7 @@ pub fn infer_and_apply_with_migration_id(
 
     let steps = run_infer_command(api, input);
 
-    apply_migration(api, steps, migration_id)
+    apply_migration(test_setup, api, steps, migration_id)
 }
 
 pub fn run_infer_command(api: &dyn GenericApi, input: InferMigrationStepsInput) -> Vec<MigrationStep> {
@@ -40,7 +41,12 @@ pub fn run_infer_command(api: &dyn GenericApi, input: InferMigrationStepsInput) 
     output.datamodel_steps
 }
 
-pub fn apply_migration(api: &dyn GenericApi, steps: Vec<MigrationStep>, migration_id: &str) -> InferAndApplyOutput {
+pub fn apply_migration(
+    test_setup: &TestSetup,
+    api: &dyn GenericApi,
+    steps: Vec<MigrationStep>,
+    migration_id: &str,
+) -> InferAndApplyOutput {
     let input = ApplyMigrationInput {
         migration_id: migration_id.to_string(),
         steps: steps,
@@ -58,14 +64,14 @@ pub fn apply_migration(api: &dyn GenericApi, steps: Vec<MigrationStep>, migratio
     );
 
     InferAndApplyOutput {
-        sql_schema: introspect_database(api),
+        sql_schema: introspect_database(test_setup, api),
         migration_output,
     }
 }
 
-pub fn unapply_migration(api: &dyn GenericApi) -> SqlSchema {
+pub fn unapply_migration(test_setup: &TestSetup, api: &dyn GenericApi) -> SqlSchema {
     let input = UnapplyMigrationInput {};
     let _ = api.unapply_migration(&input);
 
-    introspect_database(api)
+    introspect_database(test_setup, api)
 }
